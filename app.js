380fa234edfeeb9857a73a308b59a85ad390896f
Aaron Brinckerhoff <aaron.brinckerhoff@gmail.com>
2022-09-04 11:35:10 -0700

    add createListItem function

--- app.js
+++ app.js
@@ -1,13 +1,32 @@
-let listItems = [];
+const addButton = document.querySelector(".button");
+const toDoTextBox = document.querySelector(".to-do-text-box");
+const toDoList = document.querySelector(".to-do-list");
 
-const button = document.querySelector(".button");
-const textBox = document.queryselector(".to-do-text-box");
-const list = document.querySelector(".list");
+addButton.addEventListener('click', addToDo);
 
-function addListItem (text) {
-    const listItem = {
-        text: text,
-        checked: false,
-    };
-listItems.append(listItem);
+function addToDo (event) {
+
+    event.preventDefault();
+    createListItem();
+}
+
+function createListItem () {
+
+    const toDoDiv = document.createElement('div');
+    toDoDiv.classList.add('to-do-item');
+
+    const newToDo = document.createElement('li');
+    newToDo.innerText = "testing";
+    newToDo.classList.add('to-do-content');
+    toDoDiv.appendChild(newToDo);
+
+    const deleteButton = document.createElement('button');
+    deleteButton.classList.add('delete-button');
+    toDoDiv.appendChild(deleteButton);
+
+    const completeButton = document.createElement('button');
+    completeButton.classList.add('complete-button');
+    toDoDiv.appendChild(completeButton);
+
+    toDoList.appendChild(toDoDiv);
 }
